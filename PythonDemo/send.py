f0f3bdb1ebd0dc5e19b29f00682f8c3b8a8ddfaa
AlexWillBeGood <AlexWillBeGood@163.com>
2019-04-25 18:49:43 +0800

    common commit

--- PythonDemo/send.py
+++ PythonDemo/send.py
@@ -2,3 +2,7 @@
 import sys
 sys.path.append('C:\Python27\Lib\site-packages')
 import pika
+connection =pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel=connection.channel()
+
+channel.queue_declare(queue='hello')
