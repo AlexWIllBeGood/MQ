bd45dd26054f3d7ddb253709384cd61b2923ea79
AlexWillBeGood <AlexWillBeGood@163.com>
2019-06-05 15:01:38 +0800

    common commit

--- PythonDemo/ExchangesDemo20190603/Fanout/consumer1.py
+++ PythonDemo/ExchangesDemo20190603/Fanout/consumer1.py
@@ -2,10 +2,14 @@
 import pika
 
 def Main():
-    credential=pika.PlainCredentials("alex","alex")
+    # credential=pika.PlainCredentials("alex","alex")
     parameters=pika.ConnectionParameters("localhost")
-    
-
+    connection=pika.BlockingConnection(parameters)
+    channel=connection.channel()
+    channel.queue_declare(queue="test")
+    def callback(ch,method,properties,body):
+        print "%r recieved %r"%("consumer1",body)
+    channel.basic_consume(consumer_callback=callback,no_ack=True,queue="test")
 
 
 if __name__ == '__main__':
