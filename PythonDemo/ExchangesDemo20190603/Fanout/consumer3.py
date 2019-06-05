edb9bed9677b871035fe7e63585e41d9d743451a
AlexWillBeGood <AlexWillBeGood@163.com>
2019-06-05 18:33:58 +0800

    common commit

--- PythonDemo/ExchangesDemo20190603/Fanout/consumer3.py
+++ PythonDemo/ExchangesDemo20190603/Fanout/consumer3.py
@@ -2,14 +2,14 @@
 import pika
 
 def Main():
-    # credential=pika.PlainCredentials("alex","alex")
+    credential=pika.PlainCredentials("alex","alex")
     parameters=pika.ConnectionParameters("localhost")
     connection=pika.BlockingConnection(parameters)
     channel=connection.channel()
-    channel.queue_declare(queue="test1")
+    channel.queue_declare(queue="test3")
     def callback(ch,method,properties,body):
-        print "%r recieved %r"%("consumer1",body)
-    channel.basic_consume(consumer_callback=callback,no_ack=True,queue="test1")
+        print "%r received %r"%("consumer3",body)
+    channel.basic_consume(consumer_callback=callback,queue="test3",no_ack=True)
     channel.start_consuming()
 
 if __name__ == '__main__':
---
+++ PythonDemo/ExchangesDemo20190603/Fanout/producer1.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+import pika
+
+def Main():
+    credential=pika.PlainCredentials("alex","alex")
+    parameters=pika.ConnectionParameters("localhost")
+    connection=pika.BlockingConnection(parameters)
+    channel=connection.channel()
+
+    #定义queue
+    channel.queue_declare(queue="test1")
+    channel.queue_declare(queue="test2")
+    channel.queue_declare(queue="test3")
+    #定义exchange
+    channel.exchange_declare(exchange="exchange1",exchange_type="fanout")
+    #绑定queue
+    channel.queue_bind(exchange="exchange1", queue="test1")
+    channel.queue_bind(exchange="exchange1", queue="test3")
+    
+
+if __name__ == '__main__':
+    Main()
